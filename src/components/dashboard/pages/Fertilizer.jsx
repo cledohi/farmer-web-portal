fc855865325949cda5880888776dcbe2bf57bfb7
cledohi <cledohi@gmail.com>
2023-10-26 16:51:18 +0200

    fertilizer seeds and userManagement created

--- src/components/dashboard/pages/Fertilizer.jsx
+++ src/components/dashboard/pages/Fertilizer.jsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+import "./cal.css";
+import BreadCrum from "./components/breadcrum";
 function Fertilizer(props) {
-  return <div>Felitilizer</div>;
+  return (
+    <div className="main-cal container-fluid">
+      <BreadCrum title="Dashboard" subSubTitle="Fertilizer" />
+      <div className="card cal-main-card">
+        <div className="card-body">
+          <h5 className="card-title card-title-custom pb-2">Fertilizer </h5>
+          <div className="row">
+            <div className="col-md-4 col-lg-4 col-sm-12">
+              <div className="card">
+                <div className="card-body">
+                  <div className="card-title card-title-custom-sm "></div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-8 col-lg-8 col-sm-12">
+              {" "}
+              List of all Fertilizer
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Fertilizer;
--- src/components/dashboard/pages/Seeds.jsx
+++ src/components/dashboard/pages/Seeds.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import BreadCrum from "./components/breadcrum";
 
 function Seeds(props) {
-  return <div>seeds done</div>;
+  return (
+    <div className="main-cal container-fluid">
+      <BreadCrum title="Dashboard" subSubTitle="Seeds" />
+      <div className="card cal-main-card"></div>
+      <div className="card cal-main-card">
+        <div className="card-body">
+          <h5 className="card-title card-title-custom pb-2">
+            Seeds Information
+          </h5>
+          <div className="row">
+            <div className="col-md-4 col-lg-4 col-sm-12">
+              <div className="card">
+                <div className="card-body">
+                  <div className="card-title card-title-custom-sm "></div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-8 col-lg-8 col-sm-12">
+              {" "}
+              List of all users
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Seeds;
