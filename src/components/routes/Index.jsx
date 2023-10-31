f6d9ea6ecf159ee709396f0c3df24e15c0c7ffc1
cledohi <cledohi@gmail.com>
2023-10-31 13:25:15 +0200

    updated

--- src/components/routes/Index.jsx
+++ src/components/routes/Index.jsx
@@ -24,7 +24,7 @@ const FarmerRoutes = () => {
       element: <Home />,
       children: [
         { index: true, element: <Login /> },
-        { path: "/login", element: <Login /> },
+        { path: "/farmer-web-portal", element: <Login /> },
         { path: "/register", element: <RegisterUser /> },
       ],
     },
